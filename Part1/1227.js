ec4015d79b455bc7c725734a146ac2436696ee37
DoYoung Park <cym7356@naver.com>
2022-12-27 11:40:38 +0900

    좀 더 엄격한 js / this / object 내 this / arrow function

--- Part1/1227.js
+++ Part1/1227.js
@@ -26,4 +26,31 @@
 
     //기존 문법 function() {} => 신문법 ()=>{}
 
+
+    
+    document.getElementById('btn').addEventListener('click',function(e){
+        // console.log(this);
+        // console.log(e.currentTarget); 
+        //지금 이벤트 동작하는 곳.
+
+        var arr = [1,2,3];
+        arr.forEach(function(a){
+            console.log(this);
+        }); //foreach 반복문 내부코드 3번 반복
+    })
+
+
+    var object = {
+        name : ['김', '이', '박'],
+        function : function(){
+            console.log(this);
+            object.name.forEach(()=>{ 
+                //arrow function 특징 내부의 this 값을 변화시키지않음. (외부 this값 그대로 재사용 가능)
+                console.log(this);
+            })
+        }
+    }
+    object.function(); 오브젝트 함수 실행
+
+
 </script>
